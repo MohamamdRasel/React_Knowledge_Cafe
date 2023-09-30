042ca3b6e30dde1b676dfe406347f4ac03f3c4dc
Mohammad <mohammadrasel201@gmail.com>
2023-09-30 07:12:46 +0600

    bookmark added

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,17 @@ import Blogs from './Components/Blogs/Blogs'
 import Bookmarks from './Components/Bookmarks/Bookmarks'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [bookmarks, setBookmarks ] = useState([]);
 
+const handleAddToBookmark = blog =>{
+  console.log('bookmark adding soon')
+} 
   return (
     <>
 
       <Header></Header>
-      <div className='md:flex'>
-      <Blogs></Blogs>
+      <div className='md:flex max-w-7xl mx-auto'>
+      <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
       <Bookmarks></Bookmarks>
       </div>
       
